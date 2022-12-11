663d114a5d965474a074ae00da57f5527bcfe1f1
Vision Netshokotshoko <netshokotshokovision112@gmail.com>
2022-12-12 00:33:00 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,14 @@
  *
  * Return: Always 0.
  */
-
-
 int main(void)
 {
-		int i;
-		    for(i='a'; i<='z'; i++)
-			            putchar(i);
-		        	putchar('\n');
-				    return 0;
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
