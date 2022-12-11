a964ca3c24ab6ad40b12896875013069dde4a300
Vision Netshokotshoko <netshokotshokovision112@gmail.com>
2022-12-12 00:36:42 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,11 @@
  *
  * Return: Always 0.
  */
-
-
-int main() {
-
-	  int i;
-
-	  	for (i = 0; i < 10; i++)
-					putchar((i % 10) + '0');
-			putchar('\n');
-				return (0);
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+	putchar((i % 10) + '0');
+putchar('\n');
+return (0);
 }
