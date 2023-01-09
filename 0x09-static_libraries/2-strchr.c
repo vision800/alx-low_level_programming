d192d536e2d9c12471540cedfa005090c789840e
Vision Netshokotshoko <netshokotshokovision112@gmail.com>
2023-01-09 06:38:07 -0800

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 
 /**
  * _strchr - locates a character in a string
@@ -22,5 +22,5 @@ char *_strchr(char *s, char c)
 	if (c == '\0')
 		return (s);
 
-	return (NULL);
+	return (0);
 }
